b4d3919b1c5261c6d81e09efaa42205ae1dc1555
James Corr <james.corr97@gmail.com>
2022-10-08 00:29:57 +0900

    add hover popup and more custom markers

--- src/Map.js
+++ src/Map.js
@@ -7,17 +7,16 @@ import "./Map.css";
 mapboxgl.accessToken =
   "pk.eyJ1Ijoid3RnZW9ncmFwaGVyIiwiYSI6ImNrNXk3dXh6NzAwbncza3A1eHlpY2J2bmoifQ.JRy79QqtwUTYHK7dFUYy5g";
 
-const Marker = ({ onClick, children, feature }) => {
-  const _onClick = () => {
-    onClick(feature.properties.description);
-  };
-
-  return (
-    <button onClick={_onClick} className="marker">
-      {children}
-    </button>
-  );
-};
+const colors = {
+  'area 1': "#f00000",
+  'area 2': "#f30260",
+  'area 3': "#033330",
+  'area 4': "#f9f3077",
+  'area 5': "#0f8f40",
+  'area 6': "#fffff0",
+  'area 7': "#f0f040",
+  'area 8': "#f00600",
+  }
 
 const Map = () => {
   const mapContainerRef = useRef(null);
@@ -32,27 +31,27 @@ const Map = () => {
     });
 
     geoJson.features.forEach((feature) => {
-      const ref = React.createRef();
-      ref.current = document.createElement("div");
-      
-      ReactDOM.render(
-        <Marker onClick={markerClicked} feature={feature} />,
-        ref.current
-      );
-
-      new mapboxgl.Marker(ref.current)
+      const marker = new mapboxgl.Marker({color: colors[feature.properties.title]})
         .setLngLat(feature.geometry.coordinates)
-        .addTo(map);
+        .setPopup(
+          new mapboxgl.Popup({ offset: 25, closeButton: false })
+            .setHTML(
+              `<h3>${feature.properties.title}</h3><p>${feature.properties.description}</p>`
+            )
+        );
+      
+      const markerDiv = marker.getElement();
+
+      markerDiv.addEventListener('mouseenter', () => marker.togglePopup());
+      markerDiv.addEventListener('mouseleave', () => marker.togglePopup());
+
+      marker.addTo(map);
     });
 
     map.addControl(new mapboxgl.NavigationControl(), "top-right");
 
     return () => map.remove();
   }, []);
-
-  const markerClicked = (title) => {
-    window.alert(title);
-  };
 
   return <div className="map-container" ref={mapContainerRef} />;
 };
